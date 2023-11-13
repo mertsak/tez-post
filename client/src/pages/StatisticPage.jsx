f41bab2df0026d446f4e37da472a8dff9fd4ad61
Mert Sakınç <63242329+mertsak@users.noreply.github.com>
2023-11-13 18:21:58 +0300

    un ch

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,97 +1,97 @@
 import StatisticCard from "../components/Statistic/StatisticCard";
-import { Area, Pie } from "@ant-design/charts";
-import { useEffect, useState } from "react";
+// import { Area, Pie } from "@ant-design/charts";
+// import { useEffect, useState } from "react";
 
 const StatisticPage = () => {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    asyncFetch();
-  }, []);
+  // useEffect(() => {
+  //   asyncFetch();
+  // }, []);
 
-  const asyncFetch = () => {
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log("fetch data failed", error);
-      });
-  };
+  // const asyncFetch = () => {
+  //   fetch(
+  //     "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => setData(json))
+  //     .catch((error) => {
+  //       console.log("fetch data failed", error);
+  //     });
+  // };
 
-  const config = {
-    data,
-    xField: "timePeriod",
-    yField: "value",
-    xAxis: {
-      range: [0, 1],
-    },
-  };
+  // const config = {
+  //   data,
+  //   xField: "timePeriod",
+  //   yField: "value",
+  //   xAxis: {
+  //     range: [0, 1],
+  //   },
+  // };
 
-  const data2 = [
-    {
-      type: "分类一",
-      value: 27,
-    },
-    {
-      type: "分类二",
-      value: 25,
-    },
-    {
-      type: "分类三",
-      value: 18,
-    },
-    {
-      type: "分类四",
-      value: 15,
-    },
-    {
-      type: "分类五",
-      value: 10,
-    },
-    {
-      type: "其他",
-      value: 5,
-    },
-  ];
+  // const data2 = [
+  //   {
+  //     type: "分类一",
+  //     value: 27,
+  //   },
+  //   {
+  //     type: "分类二",
+  //     value: 25,
+  //   },
+  //   {
+  //     type: "分类三",
+  //     value: 18,
+  //   },
+  //   {
+  //     type: "分类四",
+  //     value: 15,
+  //   },
+  //   {
+  //     type: "分类五",
+  //     value: 10,
+  //   },
+  //   {
+  //     type: "其他",
+  //     value: 5,
+  //   },
+  // ];
 
-  const config2 = {
-    appendPadding: 10,
-    data: data2,
-    angleField: "value",
-    colorField: "type",
-    radius: 1,
-    innerRadius: 0.6,
-    label: {
-      type: "inner",
-      offset: "-50%",
-      content: "{value}",
-      style: {
-        textAlign: "center",
-        fontSize: 14,
-      },
-    },
-    interactions: [
-      {
-        type: "element-selected",
-      },
-      {
-        type: "element-active",
-      },
-    ],
-    statistic: {
-      title: false,
-      content: {
-        style: {
-          whiteSpace: "pre-wrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-        },
-        content: "AntV\nG2Plot",
-      },
-    },
-  };
+  // const config2 = {
+  //   appendPadding: 10,
+  //   data: data2,
+  //   angleField: "value",
+  //   colorField: "type",
+  //   radius: 1,
+  //   innerRadius: 0.6,
+  //   label: {
+  //     type: "inner",
+  //     offset: "-50%",
+  //     content: "{value}",
+  //     style: {
+  //       textAlign: "center",
+  //       fontSize: 14,
+  //     },
+  //   },
+  //   interactions: [
+  //     {
+  //       type: "element-selected",
+  //     },
+  //     {
+  //       type: "element-active",
+  //     },
+  //   ],
+  //   statistic: {
+  //     title: false,
+  //     content: {
+  //       style: {
+  //         whiteSpace: "pre-wrap",
+  //         overflow: "hidden",
+  //         textOverflow: "ellipsis",
+  //       },
+  //       content: "AntV\nG2Plot",
+  //     },
+  //   },
+  // };
 
   return (
     <>
@@ -131,7 +131,7 @@ const StatisticPage = () => {
             />
           </div>
 
-          <div className="flex justify-between items-center gap-10 lg:flex-row flex-col">
+          {/* <div className="flex justify-between items-center gap-10 lg:flex-row flex-col">
             <div className="lg:w-1/2 lg:h-full h-72 w-full">
               <Area {...config} />
             </div>
@@ -139,7 +139,7 @@ const StatisticPage = () => {
             <div className="lg:w-1/2 lg:h-full h-72 w-full">
               <Pie {...config2} />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
